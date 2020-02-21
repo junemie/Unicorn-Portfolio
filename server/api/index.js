3e83a58173a86e15ee555bd90ac3a56a6036f90d
June Hosoya <junehosoya@gmail.com>
2020-02-21 18:04:26 -0500

    Create api for IEX endpoint variable

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/account', require('./account'))
 router.use('/transaction', require('./transaction'))
+router.use('/iex', require('./iex'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
